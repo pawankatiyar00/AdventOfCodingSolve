5cca2afe46082ae605b99275d7f353f154087705
Pawan Katiyar <pawankatiyar00@gmail.com>
2016-03-01 09:25:39 +0530

    Create AOCd2b.java

---
+++ AOCd2b.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class AOCd2b
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String str;
+		String ar[];
+		int a=0,b=0,c=0,sum=0,small=0;
+		while(sc.hasNextLine()){
+			str=sc.nextLine();
+			ar=str.split("x");
+			a=Integer.parseInt(ar[0]);
+			b=Integer.parseInt(ar[1]);
+			c=Integer.parseInt(ar[2]);
+			if(a+b<b+c && a+b<c+a) small=a+b;
+			else if(b+c<c+a) small=b+c;
+			else small=c+a;
+			
+			sum=sum+(a*b*c)+2*small;
+			//System.out.println(Arrays.toString(ar));
+		}
+		System.out.println(sum);
+	}
+}
